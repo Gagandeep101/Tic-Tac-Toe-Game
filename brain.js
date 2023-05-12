2f3978475cc1221ce39a86cbdfb233488b581946
Gagandeep_Singh_101 <ts026925@gmail.com>
2023-05-12 18:53:00 +0530

    refactor : declare reset_game function in js

--- brain.js
+++ brain.js
@@ -4,3 +4,25 @@ var game_status = document.querySelector(".game_status");
 var player_turn = "0";
 var winner_player = "";
 var reset = document.querySelector(".replay_btn");
+
+
+function reset_game() {
+
+    // Add click eventlistener on replay button
+
+    reset.addEventListener("click", function () {
+        
+        //set game_over ,player_turn and game_status to its default values
+
+        gamer_over = false;
+        player_turn = "0";
+        game_status.innerHTML = "Player " + player_turn + " turns !!!";
+
+        // In these case we select every element(div tag under .gridbox class) in the box_input set their innerText to "" and setAttribute
+        
+        Array.from(box_input).forEach(element => {
+            element.setAttribute("style", "    background: rgba(246, 245, 245, 0.162);color: white;");
+            element.innerText = "";
+        })
+    });
+}
