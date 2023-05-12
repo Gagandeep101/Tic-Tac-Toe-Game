7167a1a19918df03599e3f06d400b6358ff9f09b
Gagandeep_Singh_101 <ts026925@gmail.com>
2023-05-12 18:45:36 +0530

    refactor : declare basic variable used in further in js

---
+++ brain.js
@@ -0,0 +1,6 @@
+var box_input = document.querySelector(".gridbox").querySelectorAll("div");
+var gamer_over = false;
+var game_status = document.querySelector(".game_status");
+var player_turn = "0";
+var winner_player = "";
+var reset = document.querySelector(".replay_btn");
